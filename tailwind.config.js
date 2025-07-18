b280d1bf9bedb1668e1d35ea6ca2713a3f1352b4
Genderson Sevilla <genderson54@gmail.com>
2025-07-18 11:28:33 -0400

    lol2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'guarico-blue': {
+          DEFAULT: '#0074D9', // Azul principal
+          dark: '#003366',   // Azul oscuro
+        },
+        'guarico-green': '#00843D', // Verde bandera
+        'guarico-yellow': '#FFD600', // Amarillo bandera
+        'guarico-black': '#000000',
+        'guarico-white': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
